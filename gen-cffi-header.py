075de5537289a4edfc6f9bf6b54661092491986a
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2025-09-13 23:45:55 +0900

    gen-cffi-header.py: emit standalone struct/union/enum definitions

--- gen-cffi-header.py
+++ gen-cffi-header.py
@@ -2,10 +2,52 @@
 
 from clang.cindex import Index, CursorKind, TypeKind
 
+def normalize_spaces(s):
+  return " ".join(s.split())
+
 def resolve_elaborated_type(t):
   if t.kind == TypeKind.ELABORATED:
     return t.get_named_type()
   return t
+
+def has_typedef_sibling(cursor):
+  parent = cursor.semantic_parent
+  for sibling in parent.get_children():
+    if sibling.kind == CursorKind.TYPEDEF_DECL:
+      t = sibling.underlying_typedef_type
+      if t.kind == TypeKind.ELABORATED:
+        t = t.get_named_type()
+      if t.get_declaration() == cursor:
+        return True
+  return False
+
+def get_fields_from_struct_or_union(decl):
+  fields = []
+  for field in decl.get_children():
+    if field.kind == CursorKind.FIELD_DECL:
+      field_type = field.type.spelling
+      field_name = field.spelling
+      fields.append(f"    {field_type} {field_name};")
+  return fields
+
+def get_constants_from_enum(decl):
+  constants = []
+  for child in decl.get_children():
+    if child.kind == CursorKind.ENUM_CONSTANT_DECL:
+      name = child.spelling
+      val  = child.enum_value
+      if val < 0x10:
+        constants.append(f"    {name} = {val},")
+      elif val < 0x100:
+        constants.append(f"    {name} = {val},")
+      elif val < 0x10000:
+        constants.append(f"    {name} = 0x{val:04X},")
+      elif val < 0x100000000:
+        constants.append(f"    {name} = 0x{val:08X},")
+      else:
+        constants.append(f"    {name} = {val},")
+  return constants
+
 
 def emit_inline_typedef_with_body(cursor):
   if cursor.kind != CursorKind.TYPEDEF_DECL:
@@ -18,28 +60,37 @@ def emit_inline_typedef_with_body(cursor):
     t = t.get_named_type()
 
   decl = t.get_declaration()
+  tag_name = decl.spelling
 
   #if t.kind not in {TypeKind.UNION, TypeKind.RECORD}:
   #  return None
 
-  if decl.kind == CursorKind.UNION_DECL:
+  if not decl.is_definition():
+    return None
+  elif decl.kind == CursorKind.UNION_DECL:
     kind_str = "union"
   elif decl.kind == CursorKind.STRUCT_DECL:
     kind_str = "struct"
+  elif decl.kind == CursorKind.ENUM_DECL:
+    kind_str = "enum"
   else:
-    kind_str = "record"
-
-  if not decl.is_definition():
+    # kind_str = "record"
     return None
 
-  fields = []
-  for field in decl.get_children():
-    if field.kind == CursorKind.FIELD_DECL:
-      field_type = field.type.spelling
-      field_name = field.spelling
-      fields.append(f"    {field_type} {field_name};")
-  body = "\n".join(fields)
+  if kind_str in {"struct", "union"}:
+    fields = get_fields_from_struct_or_union(decl)
+    body = "\n".join(fields)
+  elif kind_str == "enum":
+    constants = get_constants_from_enum(decl)
+    body = "\n".join(constants)
   return f"typedef {kind_str} {{\n{body}\n}} {typedef_name};"
+#  if tag_name:
+#    return (
+#      f"{kind_str} {tag_name} {{\n{body}\n}};\n"
+#      f"typedef {kind_str} {tag_name} {typedef_name};"
+#    )
+#  else:
+#    return f"typedef {kind_str} {{\n{body}\n}} {typedef_name};"
 
 def emit_typedef(cursor):
   if cursor.kind != CursorKind.TYPEDEF_DECL:
@@ -56,8 +107,7 @@ def emit_typedef(cursor):
   }:
     return f"typedef {t.spelling} {typedef_name};"
 
-  print(t.kind)
-  print(t.get_pointee().kind)
+  print("/* " + str(t.kind) + " " + str(t.get_pointee().kind) + " */")
 
   # Pointer to struct
   if t.kind == TypeKind.POINTER:
@@ -102,6 +152,35 @@ def emit_typedef(cursor):
 
   return None
 
+def emit_struct_union_enum_decl(cursor):
+  if cursor.kind not in { CursorKind.STRUCT_DECL, CursorKind.UNION_DECL, CursorKind.ENUM_DECL }:
+    return None
+
+  if not cursor.spelling:
+    print(f"/* anonymous structure, do not emit now */")
+    pass
+  elif has_typedef_sibling(cursor):
+    print(f"/* has sibling, do not emit now */")
+    pass
+  elif cursor.kind == CursorKind.STRUCT_DECL:
+    print(f"/* cursor.kind = {str(cursor.kind)} */")
+    fields = get_fields_from_struct_or_union(cursor)
+    body = "\n".join(fields)
+    return f"struct {cursor.spelling} {{\n{body}\n}};"
+  elif cursor.kind == CursorKind.UNION_DECL:
+    print(f"/* cursor.kind = {str(cursor.kind)} */")
+    fields = get_fields_from_struct_or_union(cursor)
+    body = "\n".join(fields)
+    return f"union {cursor.spelling} {{\n{body}\n}};"
+  elif cursor.kind == CursorKind.ENUM_DECL:
+    print(f"/* cursor.kind = {str(cursor.kind)} */")
+    constants = get_constants_from_enum(cursor)
+    body = "\n".join(constants)
+    return f"enum {cursor.spelling} {{\n{body}\n}};"
+  else:
+    print(f"/* cursor.kind = {str(cursor.kind)} */")
+    return None
+
 index = Index.create()
 tu = index.parse("test-variable.h", args = [
   "-I/opt/homebrew/opt/freetype/include/freetype2",
@@ -109,10 +188,20 @@ tu = index.parse("test-variable.h", args = [
 ])
 
 for cursor in tu.cursor.get_children():
-  print("")
   if cursor.kind.is_declaration():
-    print(cursor.spelling, cursor.kind)
+    print("/* " + str(cursor.spelling) + " " + str(cursor.kind) + " */")
 
-  typedef = emit_typedef(cursor)
-  if typedef:
-    print(typedef)
+  if cursor.kind == CursorKind.TYPEDEF_DECL:
+    typedef = emit_typedef(cursor)
+    if typedef:
+      print(typedef)
+  elif cursor.kind in { CursorKind.STRUCT_DECL, CursorKind.UNION_DECL, CursorKind.ENUM_DECL }:
+    if not cursor.is_definition():
+      print(f"/* {cursor.spelling} is not a definition */")
+      continue
+
+    str_decl = emit_struct_union_enum_decl(cursor)
+    if str_decl:
+      print(str_decl)
+
+  print("")
